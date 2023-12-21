fa37b8f65b8feba383741a52e2447b50f934b9f8
Francesco Gentile <francescogentyle@gmail.com>
2023-12-21 10:39:35 -0300

    Add vercel.json

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
-'use strict';
+"use strict";
 const app = require("./api/index.js");
 
 const port = process.env.PORT || 3000;
 
-
 app.listen(port, function () {
-  console.log('Server started on port', port);
+  console.log("Server started on port", port);
 });
